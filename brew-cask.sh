aca38294f3625e2e7717d514e008ee00c274f357
Metin Ucar <mtn.ucar@gmail.com>
2015-03-14 00:38:37 +0200

    Adds further osx tweaks and aliases

---
+++ brew-cask.sh
@@ -0,0 +1,27 @@
+brew install caskroom/cask/brew-cask
+brew tap caskroom/versions
+
+brew cask install alfred
+brew cask install bowtie
+brew cask install dropbox
+brew cask install gitx
+brew cask install sketch-toolbox
+brew cask install the-unarchiver
+brew cask install appcleaner
+brew cask install caffeine
+brew cask install firefox
+brew cask install handbrake
+brew cask install skype
+brew cask install torbrowser
+brew cask install atom
+brew cask install calibre
+brew cask install flux
+brew cask install iterm2
+brew cask install slack
+brew cask install transmission
+brew cask install audiobookbinder
+brew cask install ccleaner
+brew cask install github
+brew cask install minbox
+brew cask install spotify
+brew cask install vlc
